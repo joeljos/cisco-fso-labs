64ce62833d3707f1ca46e00e2aaf88fff8c327f4
sconrod-tester <sconrod1@gmail.com>
2022-01-21 10:37:06 -0600

    updates

--- aws_ubuntu_router_deploy.sh
+++ aws_ubuntu_router_deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-python3 auth.py > out.csv
-sed '/^$/d' out.csv > out-cln.csv
-aws configure import --csv file://out-cln.csv
-cp config ~/.aws/config
 export AWS_PAGER=""
 cp config ~/.aws
 rm -rf __pycache__
