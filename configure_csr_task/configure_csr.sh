52104b1145507f35a085ffb1500fce5089b474e4
sconrod-tester <sconrod1@gmail.com>
2022-01-29 17:13:30 -0600

    updates

--- configure_csr_task/configure_csr.sh
+++ configure_csr_task/configure_csr.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-pip install netmiko
-echo "working directory"
-pwd
-echo "directory contents"
-ls -la
-cd input
-ls -la
-echo "contents of input directory"
 chmod 400 sconrod.pem
 export AWS_PAGER=""
 rm -rf __pycache__
